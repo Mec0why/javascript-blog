6c5fc33e47f8d8f8b86b9c2880ab7abe240dbf6c
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-02-12 12:19:16 +0100

    Remove JS Comments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
-/*document.getElementById("test-button").addEventListener("click", function () {
-  const links = document.querySelectorAll(".titles a");
-  console.log("links:", links);
-});*/
-
 const titleClickHandler = function (event) {
   event.preventDefault();
   const clickedElement = this;
   console.log("Link was clicked!");
   console.log(event);
-
-  /* remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll(".titles a.active");
 
@@ -17,13 +10,9 @@ const titleClickHandler = function (event) {
     activeLink.classList.remove("active");
   }
 
-  /* add class 'active' to the clicked link */
-
   console.log("clickedElement (with plus): " + clickedElement);
 
   clickedElement.classList.add("active");
-
-  /* remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll(".posts article.active");
 
@@ -31,17 +20,11 @@ const titleClickHandler = function (event) {
     activeArticle.classList.remove("active");
   }
 
-  /* get 'href' attribute from the clicked link */
-
   const articleSelector = clickedElement.getAttribute("href");
   console.log(articleSelector);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
-
   const targetArticle = document.querySelector(articleSelector);
   console.log(targetArticle);
-
-  /* add class 'active' to the correct article */
 
   targetArticle.classList.add("active");
   console.log(targetArticle);
