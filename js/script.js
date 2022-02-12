7703462a20c92672a7c09f129ece18882df36a55
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-02-12 11:46:06 +0100

    Add JS targetArticle

--- js/script.js
+++ js/script.js
@@ -33,10 +33,13 @@ const titleClickHandler = function (event) {
 
   /* get 'href' attribute from the clicked link */
 
-  const articleSelector = clickedElement.getAttribute('href');
-  console.log(articleSelector)
+  const articleSelector = clickedElement.getAttribute("href");
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
 
   /* add class 'active' to the correct article */
 };
