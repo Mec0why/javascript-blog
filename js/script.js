9e93f5a76f2b1841c5ab4baa64f323489edf0682
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-02-12 12:21:28 +0100

    Remove JS console logs

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 const titleClickHandler = function (event) {
   event.preventDefault();
   const clickedElement = this;
-  console.log("Link was clicked!");
-  console.log(event);
 
   const activeLinks = document.querySelectorAll(".titles a.active");
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove("active");
   }
-
-  console.log("clickedElement (with plus): " + clickedElement);
 
   clickedElement.classList.add("active");
 
@@ -21,13 +17,10 @@ const titleClickHandler = function (event) {
   }
 
   const articleSelector = clickedElement.getAttribute("href");
-  console.log(articleSelector);
 
   const targetArticle = document.querySelector(articleSelector);
-  console.log(targetArticle);
 
   targetArticle.classList.add("active");
-  console.log(targetArticle);
 };
 
 const links = document.querySelectorAll(".titles a");
