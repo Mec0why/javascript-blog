6eeccfb323635f9ebcfc82ddea2865b1d77beb50
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-02-12 11:58:21 +0100

    Add JS targetArticleclass active

--- js/script.js
+++ js/script.js
@@ -42,6 +42,9 @@ const titleClickHandler = function (event) {
   console.log(targetArticle);
 
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add("active");
+  console.log(targetArticle);
 };
 
 const links = document.querySelectorAll(".titles a");
