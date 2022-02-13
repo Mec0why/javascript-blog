3be930c95c1d0f9a9c9013440174d15b25b11dc0
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-02-13 15:49:53 +0100

    Add JS linkHTML

--- js/script.js
+++ js/script.js
@@ -55,6 +55,9 @@
       console.log(articleTitle);
 
       /* create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log(linkHTML);
+
       /* insert link into titleList */
     }
   };
