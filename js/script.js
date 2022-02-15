e0f76da88722d45b113e02c1ed841a4ce2ea66d4
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-02-15 23:09:03 +0100

    Remove JS generateTags Comments + Logs

--- js/script.js
+++ js/script.js
@@ -64,48 +64,24 @@
   generateTitleLinks();
 
   const generateTags = function () {
-    /* find all articles */
-
     const articles = document.querySelectorAll(optArticleSelector);
 
-    /* START LOOP: for every article: */
-
     for (let article of articles) {
-      /* find tags wrapper */
-
       const tagWrappers = article.querySelector(optArticleTagsSelector);
-      console.log(tagWrappers);
-
-      /* make html variable with empty string */
 
       let html = '';
 
-      /* get tags from data-tags attribute */
-
       const articleTags = article.getAttribute('data-tags');
-
-      /* split tags into array */
 
       const articleTagsArray = articleTags.split(' ');
 
-      /* START LOOP: for each tag */
       for (let tag of articleTagsArray) {
-        /* generate HTML of the link */
-
         const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
 
-        /* add generated code to html variable */
-
         html = html + linkHTML;
-        console.log(html);
       }
 
-      /* END LOOP: for each tag */
-      /* insert HTML of all the links into the tags wrapper */
-
       tagWrappers.innerHTML = html;
-
-      /* END LOOP: for every article: */
     }
   };
 
